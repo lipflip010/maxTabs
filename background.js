f01d8d036de83b87aba7444d96b12e6453185f36
lipflip010 <lipflip@posteo.de>
2019-06-18 14:13:37 +0200

    finished first draft

--- background.js
+++ background.js
@@ -1,6 +1,12 @@
+const limit = 5;
+
+
 function onCreated() {
     browser.tabs.query({currentWindow: true}, (tabs) => {
-        browser.tabs.remove(tabs[0].id);
+        if(tabs.length > limit){
+            lruId = getLeastRecentlyUsedTab(tabs);
+            lruId ? browser.tabs.remove(lruId): console.log("Error: null");
+        }
     });
 }
 
@@ -8,3 +14,14 @@ browser.tabs.onCreated.addListener(() => {
     onCreated()
 });
 
+function getLeastRecentlyUsedTab(tabs) {
+    var lru  = { time: tabs[0].lastAccessed, id: null}
+
+    tabs.forEach((tab) => {
+        if(tab.lastAccessed < lru.time){
+            lru.id = tab.id;
+            lru.time  = tab.lastAccessed;
+        }
+    });
+    return lru.id;
+}
