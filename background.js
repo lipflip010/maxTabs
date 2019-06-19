e2e77ae431f6a45b60caae76910cb5017b09cb4a
lipflip010 <lipflip@posteo.de>
2019-06-19 12:44:13 +0200

    successful port

--- background.js
+++ background.js
@@ -1,26 +1,21 @@
-const limit = 5;
-
-
-function onCreated() {
-    browser.tabs.query({currentWindow: true}, (tabs) => {
-        if(tabs.length > limit){
-            lruId = getLeastRecentlyUsedTab(tabs);
-            lruId ? browser.tabs.remove(lruId): console.log("Error: null");
+"use strict";
+var limit = 5;
+var onCreated = function () {
+    browser.tabs.query({ currentWindow: true })
+        .then(function (tabs) {
+        if (tabs.length > limit) {
+            var lruId = getLeastRecentlyUsedTab(tabs);
+            lruId ? browser.tabs.remove(lruId) : console.log("Error: null");
         }
     });
-}
-
-browser.tabs.onCreated.addListener(() => {
-    onCreated()
-});
-
+};
+browser.tabs.onCreated.addListener(onCreated);
 function getLeastRecentlyUsedTab(tabs) {
-    var lru  = { time: tabs[0].lastAccessed, id: null}
-
-    tabs.forEach((tab) => {
-        if(tab.lastAccessed < lru.time){
+    var lru = { time: tabs[0].lastAccessed, id: tabs[0].id };
+    tabs.forEach(function (tab) {
+        if (tab.lastAccessed && lru.time && tab.lastAccessed < lru.time) {
             lru.id = tab.id;
-            lru.time  = tab.lastAccessed;
+            lru.time = tab.lastAccessed;
         }
     });
     return lru.id;
